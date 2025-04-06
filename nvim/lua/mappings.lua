d426b7eac8afb1b0416e984347c9be4edea4c6e1
ewoifuoi <hamimelons@126.com>
2025-04-06 19:28:41 +0800

    增加一键执行uv run

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -9,16 +9,30 @@ map("i", "jj", "<ESC>")
 map("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", {desc = "Close NvimTree"})
 
 
+local function find_pyproject()
+  local path = vim.fn.expand("%:p:h")
+  while path ~= "/" do
+    if vim.fn.filereadable(path .. "/pyproject.toml") == 1 then
+      return true
+    end
+    path = vim.fn.fnamemodify(path, ":h")
+  end
+  return false
+end
 
 map({"n", "i"},"<F5>", function ()
   local filetype = vim.bo.filetype
   local filename = vim.fn.expand("%:p")
   local cmd = ""
-  --自动保存文件
+
   vim.cmd("w")
 
   if filetype == "python" then
-    cmd = "python " .. filename
+    if find_pyproject() then
+      cmd = "uv run " .. filename
+    else
+      cmd = "python " .. filename
+    end
   end
   vim.cmd("split | terminal " .. cmd)
   vim.cmd("startinsert")
