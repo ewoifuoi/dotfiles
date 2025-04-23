8409cf5f8d706ff25e4c41599ed037fb7afee374
ewoifuoi <hamimelons@126.com>
2025-04-23 15:51:44 +0800

    nvim半透明

--- nvim/lua/chadrc.lua
+++ nvim/lua/chadrc.lua
@@ -6,8 +6,9 @@
 local M = {}
 
 M.base46 = {
-	theme = "aquarium",
+	theme = "catppuccin",
 
+  transparency = true,
 	-- hl_override = {
 	-- 	Comment = { italic = true },
 	-- 	["@comment"] = { italic = true },
