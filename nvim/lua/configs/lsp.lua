18102d5fb88df3f62526f8f66168c973b2da2cbd
ewoifuoi <arum1kuy1@outlook.com>
2026-04-07 13:35:07 +0800

    fix vim deprecated issue

--- nvim/lua/configs/lsp.lua
+++ nvim/lua/configs/lsp.lua
@@ -1,13 +1,86 @@
-local lspconfig = require "lspconfig"
-local nvlsp = require "nvchad.configs.lspconfig"
+local map = vim.keymap.set
 
-lspconfig.clangd.setup {
-  on_attach = function(client, bufnr)
-    client.server_capabilities.signatureHelpProvider = false
-    nvlsp.on_attach(client, bufnr)
+if not vim.lsp.config then
+  vim.notify("This LSP config requires Neovim 0.11+", vim.log.levels.ERROR)
+  return
+end
+
+local function on_attach(_, bufnr)
+  local function opts(desc)
+    return { buffer = bufnr, desc = "LSP " .. desc }
+  end
+
+  map("n", "gD", vim.lsp.buf.declaration, opts "Go to declaration")
+  map("n", "gd", vim.lsp.buf.definition, opts "Go to definition")
+  map("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts "Add workspace folder")
+  map("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts "Remove workspace folder")
+  map("n", "<leader>wl", function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, opts "List workspace folders")
+  map("n", "<leader>D", vim.lsp.buf.type_definition, opts "Go to type definition")
+  map("n", "<leader>ra", require "nvchad.lsp.renamer", opts "NvRenamer")
+end
+
+local function on_init(client, _)
+  if client:supports_method "textDocument/semanticTokens" then
+    client.server_capabilities.semanticTokensProvider = nil
+  end
+end
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+
+capabilities.textDocument.completion.completionItem = {
+  documentationFormat = { "markdown", "plaintext" },
+  snippetSupport = true,
+  preselectSupport = true,
+  insertReplaceSupport = true,
+  labelDetailsSupport = true,
+  deprecatedSupport = true,
+  commitCharactersSupport = true,
+  tagSupport = { valueSet = { 1 } },
+  resolveSupport = {
+    properties = {
+      "documentation",
+      "detail",
+      "additionalTextEdits",
+    },
+  },
+}
+
+pcall(dofile, vim.g.base46_cache .. "lsp")
+require("nvchad.lsp").diagnostic_config()
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("user_lsp_attach", { clear = true }),
+  callback = function(args)
+    on_attach(nil, args.buf)
   end,
-  on_init = nvlsp.on_init,
-  capabilities = nvlsp.capabilities,
+})
+
+vim.lsp.config("*", {
+  capabilities = capabilities,
+  on_init = on_init,
+})
+
+vim.lsp.config("lua_ls", {
+  settings = {
+    Lua = {
+      workspace = {
+        library = {
+          vim.fn.expand "$VIMRUNTIME/lua",
+          vim.fn.stdpath "data" .. "/lazy/ui/nvchad_types",
+          vim.fn.stdpath "data" .. "/lazy/lazy.nvim/lua/lazy",
+          "${3rd}/luv/library",
+        },
+      },
+    },
+  },
+})
+
+vim.lsp.config("clangd", {
+  on_attach = function(client, _)
+    client.server_capabilities.signatureHelpProvider = false
+  end,
   cmd = {
     "clangd",
     "--background-index",
@@ -17,4 +90,7 @@ lspconfig.clangd.setup {
     "--function-arg-placeholders",
     "--fallback-style=llvm",
   },
-}
+})
+
+vim.lsp.enable "lua_ls"
+vim.lsp.enable "clangd"
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -5,7 +5,6 @@ return {
   {
     "neovim/nvim-lspconfig",
     config = function()
-      require("nvchad.configs.lspconfig").defaults()
       require("configs.lsp")
     end,
   },
